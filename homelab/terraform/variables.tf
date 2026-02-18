2a75f462af072c60c73ec819e2b93328f4d422da
SteliosSpanos <stylspan19@gmail.com>
2026-02-18 14:06:37 +0200

    Additions of the new features

--- homelab/terraform/variables.tf
+++ homelab/terraform/variables.tf
@@ -37,11 +37,35 @@ variable "instance_types" {
     jump_box = string
     nat      = string
     main_vm  = string
+    web_app = string
   })
   default = {
     jump_box = "t3.micro"
     nat      = "t3.micro"
     main_vm  = "t3.micro"
+    web_app = "t3.micro"
   }
   description = "EC2 instance types for each instance"
+}
+
+variable "db_instance_class" {
+  type = string
+  default = "db.t3.micro"
+  description = "Instance class of RDS"
+}
+
+variable "db_name" {
+  type = string
+  description = "Name of the RDS Database"
+}
+
+variable "db_username" {
+  type = string
+  description = "Master username for RDS"
+}
+
+variable "db_password" {
+  type = string
+  sensitive = true
+  description = "Master password for RDS"
 }
