fc3e10402a5758a7b6d569f7d709d459fc3fbcda
SteliosSpanos <stylspan19@gmail.com>
2026-02-27 23:40:46 +0200

    Create new project with three-tier app

---
+++ three-tier-app/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  type        = string
+  description = "AWS CLI profile"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "three-tier-app"
+  description = "Project name for resource naming"
+}
