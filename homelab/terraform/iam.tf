6e8a5fca8e30549100732de69c84b940d1ea904a
SteliosSpanos <stylspan19@gmail.com>
2026-03-02 23:31:17 +0200

    Creates seperate iam policies for each instance

--- homelab/terraform/iam.tf
+++ homelab/terraform/iam.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role" "jump_box" {
 
 resource "aws_iam_role_policy_attachment" "jump_box_cloudwatch" {
   role       = aws_iam_role.jump_box.name
-  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+  policy_arn = aws_iam_policy.jump_box_cloudwatch.arn
 }
 
 resource "aws_iam_role_policy_attachment" "jump_box_ssm" {
@@ -26,7 +26,7 @@ resource "aws_iam_role" "nat_instance" {
 
 resource "aws_iam_role_policy_attachment" "nat_instance_cloudwatch" {
   role       = aws_iam_role.nat_instance.name
-  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+  policy_arn = aws_iam_policy.nat_instance_cloudwatch.arn
 }
 
 resource "aws_iam_role_policy_attachment" "nat_instance_ssm" {
@@ -52,7 +52,7 @@ resource "aws_iam_role_policy_attachment" "main_vm_s3" {
 
 resource "aws_iam_role_policy_attachment" "main_vm_cloudwatch" {
   role       = aws_iam_role.main_vm.name
-  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+  policy_arn = aws_iam_policy.main_vm_cloudwatch.arn
 }
 
 resource "aws_iam_role_policy_attachment" "main_vm_ssm" {
@@ -73,7 +73,7 @@ resource "aws_iam_role" "web_app" {
 
 resource "aws_iam_role_policy_attachment" "web_app_cloudwatch" {
   role       = aws_iam_role.web_app.name
-  policy_arn = aws_iam_policy.cloudwatch_logs.arn
+  policy_arn = aws_iam_policy.web_app_cloudwatch.arn
 }
 
 resource "aws_iam_role_policy_attachment" "web_app_ssm" {
@@ -84,28 +84,6 @@ resource "aws_iam_role_policy_attachment" "web_app_ssm" {
 resource "aws_iam_instance_profile" "web_app" {
   name = "${var.project_name}-web-app-profile"
   role = aws_iam_role.web_app.name
-}
-
-
-resource "aws_iam_policy" "cloudwatch_logs" {
-  name        = "${var.project_name}-cloudwatch-logs-policy"
-  description = "Allow EC2 instances to write to CloudWatch Logs"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
 }
 
 resource "aws_iam_policy" "s3_access" {
@@ -126,6 +104,98 @@ resource "aws_iam_policy" "s3_access" {
         Resource = [
           aws_s3_bucket.homelab.arn,
           "${aws_s3_bucket.homelab.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "jump_box_cloudwatch" {
+  name        = "${var.project_name}-jump-box-cloudwatch-policy"
+  description = "Allow jump box to write to its own CloudWatch log group"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = [
+          aws_cloudwatch_log_group.jump_box.arn,
+          "${aws_cloudwatch_log_group.jump_box.arn}:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "nat_instance_cloudwatch" {
+  name        = "${var.project_name}-nat-instance-cloudwatch-policy"
+  description = "Allow nat instance to write to its own CloudWatch log group"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = [
+          aws_cloudwatch_log_group.nat_instance.arn,
+          "${aws_cloudwatch_log_group.nat_instance.arn}:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "main_vm_cloudwatch" {
+  name        = "${var.project_name}-main-vm-cloudwatch-policy"
+  description = "Allow main vm to write to its own CloudWatch log group"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = [
+          aws_cloudwatch_log_group.main_vm.arn,
+          "${aws_cloudwatch_log_group.main_vm.arn}:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "web_app_cloudwatch" {
+  name        = "${var.project_name}-web-app-cloudwatch-policy"
+  description = "Allow web app to write to its own CloudWatch log group"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = [
+          aws_cloudwatch_log_group.web_app.arn,
+          "${aws_cloudwatch_log_group.web_app.arn}:*"
         ]
       }
     ]
