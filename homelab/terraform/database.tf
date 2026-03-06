3d0d8a4df21c7bb0fcacd1601e4aa097cff5640c
SteliosSpanos <stylspan19@gmail.com>
2026-03-06 23:47:26 +0200

    Adds kms key to postgres

--- homelab/terraform/database.tf
+++ homelab/terraform/database.tf
@@ -27,7 +27,9 @@ resource "aws_db_instance" "postgres" {
   vpc_security_group_ids = [aws_security_group.postgres.id]
   parameter_group_name   = aws_db_parameter_group.postgres.name
 
-  storage_encrypted       = true
+  storage_encrypted = true
+  kms_key_id        = aws_kms_key.homelab.arn
+
   deletion_protection     = false
   skip_final_snapshot     = true
   backup_retention_period = 1
