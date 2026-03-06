020c77fb2725e755656aa874d75ef8baf41c66e3
SteliosSpanos <stylspan19@gmail.com>
2026-03-07 00:56:21 +0200

    Changes required version

--- homelab/terraform/providers.tf
+++ homelab/terraform/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5, < 2.0"
 
   required_providers {
     aws = {
@@ -22,4 +22,10 @@ terraform {
 provider "aws" {
   profile = var.profile
   region  = var.region
+
+  default_tags {
+    tags = {
+      Project = var.project_name
+    }
+  }
 }
