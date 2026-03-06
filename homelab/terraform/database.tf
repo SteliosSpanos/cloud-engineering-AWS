eeae78107e00eacd9cb0025aef8c4c7098b4bac7
SteliosSpanos <stylspan19@gmail.com>
2026-03-07 00:55:39 +0200

    Forces TLS connections only

--- homelab/terraform/database.tf
+++ homelab/terraform/database.tf
@@ -9,6 +9,12 @@ resource "aws_db_subnet_group" "postgres" {
 resource "aws_db_parameter_group" "postgres" {
   family = "postgres15"
   name   = "${var.project_name}-db-pg"
+
+  parameter {
+    name         = "rds.force_ssl"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
 }
 
 resource "aws_db_instance" "postgres" {
@@ -21,11 +27,14 @@ resource "aws_db_instance" "postgres" {
 
   db_name  = var.db_name
   username = var.db_username
-  password = var.db_password
+
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.homelab.arn
 
   db_subnet_group_name   = aws_db_subnet_group.postgres.name
   vpc_security_group_ids = [aws_security_group.postgres.id]
   parameter_group_name   = aws_db_parameter_group.postgres.name
+  publicly_accessible    = false
 
   storage_encrypted = true
   kms_key_id        = aws_kms_key.homelab.arn
