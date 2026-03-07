3b2d7152c788f821da9aefbab33ffa93a2c15bd0
SteliosSpanos <stylspan19@gmail.com>
2026-03-07 10:49:16 +0200

    Removes tls option in provider

--- homelab/terraform/providers.tf
+++ homelab/terraform/providers.tf
@@ -1,15 +1,20 @@
 terraform {
   required_version = ">= 1.5, < 2.0"
 
+  backend "s3" {
+    bucket         = "homelab-tfstate-487322974754"
+    key            = "terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "homelab-tfstate-lock"
+    encrypt        = true
+  }
+
+  // Should do: export AWS_PROFILE=<profile-name>
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
     }
 
     local = {
