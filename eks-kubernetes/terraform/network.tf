4586f0378511b50bce4e230d5d387484629e2108
SteliosSpanos <stylspan19@gmail.com>
2026-03-09 10:58:47 +0200

    Creates networking for bastion host

--- eks-kubernetes/terraform/network.tf
+++ eks-kubernetes/terraform/network.tf
@@ -8,18 +8,33 @@ resource "aws_vpc" "cluster_vpc" {
   }
 }
 
-resource "aws_subnet" "cluster_public_subnet" {
+resource "aws_subnet" "public" {
+  count                   = 2
   vpc_id                  = aws_vpc.cluster_vpc.id
-  cidr_block              = var.subnet_cidr
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "${var.project_name}-public-subnet"
+    Name                     = "${var.project_name}-public"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
-resource "aws_internet_gateway" "cluster-igw" {
+resource "aws_subnet" "private" {
+  count                   = 2
+  vpc_id                  = aws_vpc.cluster_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index + 2)
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                              = "${var.project_name}-private"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.cluster_vpc.id
 
   tags = {
@@ -27,7 +42,26 @@ resource "aws_internet_gateway" "cluster-igw" {
   }
 }
 
-resource "aws_route_table" "cluster_public_rt" {
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.cluster_public[0].id
+
+  depends_on = [aws_internet_gateway.cluster-igw]
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+}
+
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.cluster_vpc.id
 
   tags = {
@@ -35,13 +69,28 @@ resource "aws_route_table" "cluster_public_rt" {
   }
 }
 
-resource "aws_route_table_association" "cluster_public_assoc" {
+resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.cluster_public_subnet.id
   route_table_id = aws_route_table.cluster_public_rt.id
 }
 
-resource "aws_route" "route_to_igw" {
+resource "aws_route" "public_igw" {
   route_table_id         = aws_route_table.cluster_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.cluster-igw.id
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.cluster_vpc.id
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
