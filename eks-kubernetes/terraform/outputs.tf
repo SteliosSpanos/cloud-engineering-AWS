2807ddc7250efccfd58c2c77c828b1725ea26b50
SteliosSpanos <stylspan19@gmail.com>
2026-03-09 10:57:39 +0200

    Creates the most useful outputs

---
+++ eks-kubernetes/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API server endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "bation_public_ip" {
+  description = "Bastion EC2 public IP"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "kubeconfig_command" {
+  description = "Run this on the bastion to configure kubectl"
+  value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.this.name} --region ${var.region}"
+}
