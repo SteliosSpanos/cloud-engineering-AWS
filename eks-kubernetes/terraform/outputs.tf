8ea418c5f56ecb1842575c6de41e5ac59b92ab21
SteliosSpanos <stylspan19@gmail.com>
2026-03-10 16:38:18 +0200

    Include repo URL

--- eks-kubernetes/terraform/outputs.tf
+++ eks-kubernetes/terraform/outputs.tf
@@ -17,3 +17,17 @@ output "kubeconfig_command" {
   description = "Run this on the bastion to configure kubectl"
   value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.this.name} --region ${var.region}"
 }
+
+output "ecr_repository_url" {
+  description = "ECR repository URL for docker push"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "docker_push_commands" {
+  description = "Commands to authenticate and push the image"
+  value       = <<-EOT
+    aws ecr get-login-password --profile ${var.profile} --region ${var.region} | docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+    docker build -t ${aws_ecr_repository.app.repository_url}:latest ./nextwork-flask-backend
+    docker push ${aws_ecr_repository.app.repository_url}:latest
+  EOT
+}
